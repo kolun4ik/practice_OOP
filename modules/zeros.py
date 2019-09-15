fa3ffd2af41de7436e6a59b24d474218e2af6ef0
kolun4ik <11_ka@rambler.ru>
2019-09-15 17:39:18 +0300

    add optimized trailing zeros calculate function

--- modules/zeros.py
+++ modules/zeros.py
@@ -1,5 +1,6 @@
 import time
 
+
 def zeros(n):
     """calculate the number of trailing zeros
     in a factorial of a given number"""
@@ -12,13 +13,45 @@ def zeros(n):
     return count_zero - 1
 
 
+# Algorithm for Counting trailing zeros in factorial of a number.
+# So in general, if you want to count trailing zero in factorial of a number,
+# you have to,
+#
+# 1. Divide the number by 5, to find out how much 5 factors are present, then,
+# 2. Divide the number by 25 to find out how many times 25 are present
+# in a number as it will add extra 5 to number then,
+# 3. Divide the number by 125 to find out how many times 125 are present
+# in a number as it will add extra 5 to number and so on.
+
+
+def zeros_optimized(n):
+    '''Optimized calculate the number of trailing zeros
+    in a factorial of a given number'''
+    i = 1
+    count_zero = 0
+    while n >= i:
+        i *= 5
+        count_zero += int(n / i)
+    return count_zero
+
+
 if __name__ == "__main__":
     n = 50
-    t = 0
+    t1 = t2 = 0
     for i in range(n):
-        start = time.perf_counter()
-        z = zeros(30)
-        end = time.perf_counter()
-        t += end - start
-    print('У факториала({}) -  {} нулей в конце!'.format(30, z))
-    print('Среднее ремя выполнения = ', t / n)
+        start1 = time.perf_counter()
+        z = zeros(1000)
+        end1 = time.perf_counter()
+        t1 += end1 - start1
+
+    for i in range(n):
+        start2 = time.perf_counter()
+        z = zeros_optimized(1000)
+        end2 = time.perf_counter()
+        t2 += end2 - start2
+
+    print('У факториала({}) -  {} нулей в конце!'.format(1000, z))
+    print('Среднее ремя выполнения: ')
+    print('\tzeros function = ', t1 / n)
+    print('\tzeros_optimized function = ', t2 / n)
+    print('time gain:', t1 - t2)
--- tests/test_counter_zeros.py
+++ tests/test_counter_zeros.py
@@ -1,13 +1,14 @@
 import unittest
-from modules.zeros import zeros
+from modules.zeros import zeros, zeros_optimized
 from unittest import skip
+
 
 class TestZerosFunction(unittest.TestCase):
     """Test function calculate the number of trailing zeros"""
     def test_calculate_none_trailing_zeros(self):
         """test: return  none trailing zeros"""
         self.assertEqual(zeros(0), 0)
-    
+
     def test_calculate_one_trailing_zeros(self):
         """test: return one trailing zeros"""
         self.assertEqual(zeros(6), 1)
@@ -15,3 +16,22 @@ class TestZerosFunction(unittest.TestCase):
     def test_calculate_seven_trailing_zeros(self):
         """test: return seven trailing zeros"""
         self.assertEqual(zeros(30), 7)
+
+
+class TestCountZerosOptimized(unittest.TestCase):
+    """Test optimized function calculate trailing zeros"""
+
+    def test_optimized_equals_calc_zeros(self):
+        """test: optimized function calc zeros equals
+        calc zeros function"""
+        self.assertEqual(zeros_optimized(0), zeros(0))
+
+    def test_optimized_equals_calc_zeros_with_args_six(self):
+        """test: optimized function calc zeros equals
+        calc zeros function with arg = 6"""
+        self.assertEqual(zeros_optimized(6), zeros(6))
+
+    def test_optimized_equals_calc_zeros_with_args_thirty(self):
+        """test: optimized function calc zeros equals
+        calc zeros function with arg = 30"""
+        self.assertEqual(zeros_optimized(30), zeros(30))
