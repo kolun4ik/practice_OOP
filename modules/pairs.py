2bd6374cdb68499695d150de9acad0ea9efdae34
kolun4ik <11_ka@rambler.ru>
2019-09-22 22:04:55 +0300

    add sum pairs fn and tests suit him

---
+++ modules/pairs.py
@@ -0,0 +1,15 @@
+def sum_pairs(ints, s):
+    len_ints = indice = len(ints)
+    pairs = []
+    for i in range(len_ints):
+        for j in range(i + 1, len_ints):
+            if ints[i] + ints[j] == s and j <= indice:
+                indice = j
+                pairs.clear()
+                pairs.append(ints[i])
+                pairs.append(ints[j])
+
+    if len(pairs):
+        return pairs
+
+    # [10, 5, 2, 3, 7, 5]
